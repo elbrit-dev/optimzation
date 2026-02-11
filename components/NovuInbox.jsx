7540b7c7a489ba711108e0b91637d66afb867588
barani0709 <baranidharan@elbrit.org>
2026-02-11 17:39:20 +0530

    updated novu

--- components/NovuInbox.jsx
+++ components/NovuInbox.jsx
@@ -50,71 +50,16 @@ const NovuInbox = ({
   }
 
   const tabs = [
-    {
-      label: "All",
-      filter: {},
-    },
-    {
-      label: "Approval",
-      filter: {
-        tags: ["approval"],
-      },
-    },
-    {
-      label: "Announcement",
-      filter: {
-        tags: ["announcement"],
-      },
-    },
+    { label: "All", filter: {} },
+    { label: "Approval", filter: { tags: ["approval"] } },
+    { label: "Announcement", filter: { tags: ["announcement"] } },
   ];
 
-  if (mode === "icon") {
-    return (
-      <div
-        className={className}
-        style={{
-          display: "inline-flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        {...props}
-      >
-        <NovuProvider {...novuProviderProps}>
-          <Inbox tabs={tabs} />
-        </NovuProvider>
-      </div>
-    );
-  }
-
   return (
-    <div
-      className={className}
-      style={{
-        minHeight: "100vh",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "20px",
-        boxSizing: "border-box",
-      }}
-      {...props}
-    >
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "420px",
-          height: "80vh",
-          maxHeight: "700px",
-          overflow: "hidden",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <NovuProvider {...novuProviderProps}>
-          <Inbox tabs={tabs} />
-        </NovuProvider>
-      </div>
+    <div className={className} {...props}>
+      <NovuProvider {...novuProviderProps}>
+        <Inbox tabs={tabs} />
+      </NovuProvider>
     </div>
   );
 };
